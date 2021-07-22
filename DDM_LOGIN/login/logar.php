c4190ebc8b50af54cb430ce89314351cfac1b71b
Giovanna_Alves <64507346+Giovanna-Alves15@users.noreply.github.com>
2021-07-22 19:38:16 -0300

    Login-upl01 22/07/21

---
+++ DDM_LOGIN/login/logar.php
@@ -0,0 +1,17 @@
+<?php
+    include_once 'conexao.php';
+
+    $email = $_POST['email'];
+    $senha = $_POST['senha'];
+
+    //executar uma pesquisa no banco de dados
+    $sql = $dbcon->query("SELECT * FROM tblogin WHERE email ='$email' AND senha ='$senha'");
+
+    if(mysqli_num_rows($sql)>0){
+        echo "login_ok";
+    }else{
+        echo "login_erro";
+    }
+
+
+?>
